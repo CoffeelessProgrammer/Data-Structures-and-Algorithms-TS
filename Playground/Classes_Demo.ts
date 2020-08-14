6535ce94515f75b4723be4041d1dbdd2313706c8
Ryo Light <coffeelessprogrammer@gmail.com>
2020-08-14 14:43:28 -0400

    Example of using classes in TypeScript

---
+++ Playground/Classes_Demo.ts
@@ -0,0 +1,45 @@
+// RUN: deno run Playground/Classes_Demo.ts
+
+class Bender {
+  protected name: string;
+  protected isFemale: boolean;
+  protected element: string;
+
+  constructor(name: string, isFemale: boolean, element: string) {
+    this.name = name;
+    this.isFemale = isFemale;
+    this.element = element;
+
+    // console.log('bender', this);
+  }
+
+  public introduce() {
+    console.log(`"Hi, I am ${this.name}. I'm from the ${this.element} nation!"`);
+  }
+}
+
+class EarthBender extends Bender {
+  protected specialty?: string;
+
+  constructor(name: string, isFemale: boolean, specialty?: string) {
+    super(name, isFemale, 'Earth');
+    this.specialty = specialty;
+
+    // console.log('EarthBender', this);
+  }
+
+  public earthbend() {
+    console.log(`${this.name} lifts the ground beneath ${this.isFemale ? 'her' : 'him'}!`);
+  }
+}
+
+
+
+const toph = new EarthBender('Toph', true, 'metal');
+const bumi = new EarthBender('Bumi', false);
+
+toph.introduce();
+toph.earthbend();
+
+bumi.introduce();
+bumi.earthbend();
